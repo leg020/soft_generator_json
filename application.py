17b498e7bfd8447788f0f72d9723d5bffbc7b1e8
Александр <leg020@inbox.ru>
2020-07-19 19:52:56 +0300

    Создана старница для создания Json - задания

--- application.py
+++ application.py
@@ -8,5 +8,10 @@ def get_main_page():
     return render_template('main.html')
 
 
+@flask.route('/new_test', methods=["GET"])
+def get_new_test_page():
+    return render_template('new_test.html')
+
+
 if __name__ == '__main__':
     flask.run(host="127.0.0.1", port="7000", debug=True, threaded=True, use_reloader=False)
