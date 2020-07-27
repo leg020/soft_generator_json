4c670d83b0fa34e6ff12ff2bfc95dcc179640096
Александр <leg020@inbox.ru>
2020-07-27 16:01:17 +0300

    Добавил кнопки на страницу для создания тестов

--- application.py
+++ application.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template
+from fixture.db import DataBase
 
 flask = Flask(__name__)
 
 
 @flask.route('/', methods=["GET"])
 def get_main_page():
-    return render_template('main.html')
+    tests = get_data_for_main_page()
+    return render_template('main.html', data=tests)
 
 
 @flask.route('/new_test', methods=["GET"])
 def get_new_test_page():
     return render_template('new_test.html')
+
+
+def get_data_for_main_page():
+    db = DataBase(host='127.0.0.1', name='tests', user='root', password='')
+    data = db.get_tests()
+    return data
+
+
 
 
 if __name__ == '__main__':
